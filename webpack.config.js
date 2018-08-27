73de0a88b3be1efc039d1ac082627d7b0d7a3416
FengMind <fgdata@qq.com>
2018-08-27 19:04:04 +0800

    by-flex v0.9 Flex布局的24栅格系统实现 百优专用

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "js/index.js",
+    },
+
+    //以下是服务环境配置
+    devServer: {
+        contentBase: "./dist",
+        inline: true//实时刷新
+    },
+
+    module: {
+        rules:[
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use:[
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                use:[
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: "./images/[name].[hash:7].[ext]"
+                    }
+                }
+            },
+            {
+                test: /\.(eot|woff|ttf|woff2)(\?|$)/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        limit: 10000,
+                        name: "./images/[name].[hash:7].[ext]"
+                    }
+                }
+            }
+        ]
+    }
+};
