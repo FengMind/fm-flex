2303a654070e1bccb6c41a028d38b8363d3426ad
FengMind <fgdata@qq.com>
2018-08-31 18:41:18 +0800

    前缀

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({
-            browsers: ['last 5 version',"> 1%"] //前五种浏览器版本
+            browsers: ['last 5 version',"> 1%"]  //前五种浏览器版本
         })
     ]
 };
